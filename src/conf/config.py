bc9d0995f9554572431c86c0e1c44a85f9762d2b
lexxai <lexxai@users.noreply.github.com>
2023-12-12 16:59:40 +0200

    default values for config

--- src/conf/config.py
+++ src/conf/config.py
@@ -4,24 +4,22 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     sqlalchemy_database_url: str | None = None
 
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int
-    mail_server: str
+    mail_username: str = ""
+    mail_password: str = ""
+    mail_from: str = ""
+    mail_port: int = 465
+    mail_server: str = "localhost"
 
-    redis_host: str
-    redis_port: int
+    redis_host: str = "localhost"
+    redis_port: int = 6379
 
-    cloudinary_name: str
-    cloudinary_api_key: str
-    cloudinary_api_secret: str
-
+    cloudinary_name: str = ""
+    cloudinary_api_key: str = ""
+    cloudinary_api_secret: str = ""
 
     model_config = SettingsConfigDict(
         extra="ignore", env_file=".env", env_file_encoding="utf-8"
     )
-
 
 
 settings = Settings()
