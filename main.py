e2614116ae8d0232eaab0a0ba7cd61f9be630342
lexxai <lexxai@users.noreply.github.com>
2023-12-12 17:06:06 +0200

    password=settings.redis_password limiter

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from src.conf.config import settings
 
 app = FastAPI()
 
-app.include_router(users.router, prefix='/api')
-app.include_router(auth.router, prefix='/api')
+app.include_router(users.router, prefix="/api")
+app.include_router(auth.router, prefix="/api")
 
 
 app.add_middleware(
@@ -30,9 +30,16 @@ async def startup():
     :return: None.
     :rtype: None
     """
-    r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0, encoding="utf-8",
-                          decode_responses=True)
+    r = await redis.Redis(
+        host=settings.redis_host,
+        port=settings.redis_port,
+        password=settings.redis_password,
+        db=0,
+        encoding="utf-8",
+        decode_responses=True,
+    )
     await FastAPILimiter.init(r)
+
 
 @app.get("/")
 def read_root():
