df1f06d83e5feedb90e07c13f2e939fa456c8f92
AlexanderBgit <sb4mymail@gmail.com>
2023-12-14 16:29:04 +0200

    image to Postgre has broken

---
+++ src/repository/logout.py
@@ -0,0 +1,63 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import delete, select
+from datetime import date, timedelta
+
+from src.database.models import Bannedlist
+
+
+async def add_token(token: str|None, db: Session) -> int | None:
+    """Add token to black list 
+
+    :param token: auth_token
+    :type token: str | None
+    :param db: Database session connection
+    :type db: Session
+    :return: id of added token
+    :rtype: int | None
+    """
+    if token:
+        try:
+            obj = Bannedlist(token=token)
+            db.add(obj)
+            db.commit()
+            db.refresh(obj)
+            return obj.id # type: ignore
+        except Exception as err:
+            print(f"DB error {err=}")
+            return None
+
+
+async def check_token(token: str, db: Session) -> bool:
+    """Chack token present on database or no
+
+    :param token: auth_token
+    :type token: str
+    :param db: Database session connection
+    :type db: Session
+    :return: True if token is present on database
+    :rtype: bool
+    """
+    stmt = select(Bannedlist.id).where(Bannedlist.token==token)
+    result = db.scalar(stmt)
+    # print(f"{result=}")
+    # result = db.query(Bannedlist).filter_by(token=token).first()
+    return result is not None
+
+
+async def purge_old(db: Session, duration: int = 7) -> int:
+    """_summary_
+
+    :param db: Purge old records for expired tokens
+    :type db: Session
+    :param duration: Day how old token will be purged, defaults to 7
+    :type duration: int, optional
+    :return: How many recods was deleted
+    :rtype: int
+    """
+    start_range = date.today() + timedelta(days=-duration)
+    stmt = delete(Bannedlist).where(Bannedlist.created_at <= start_range)
+    result = db.execute(stmt)
+    db.commit()
+    return result.rowcount
+
+
---
+++ src/repository/users.py
@@ -0,0 +1,96 @@
+from libgravatar import Gravatar
+from sqlalchemy.orm import Session
+
+from src.database.models import User, Role
+from src.schemas import UserModel
+
+
+async def get_user_by_email(email: str, db: Session) -> User:
+    """
+    Retrieves a user by his email.
+
+    :param email: An email to get user from the database by.
+    :type email: str
+    :param db: The database session.
+    :type db: Session
+    :return: The user.
+    :rtype: User
+    """ 
+    return db.query(User).filter(User.email == email).first()
+
+
+async def create_user(body: UserModel, db: Session) -> User:
+    """
+    Creates a new user.
+
+    :param body: The data for the user to create.
+    :type body: UserModel
+    :param db: The database session.
+    :type db: Session
+    :return: The newly created user.
+    :rtype: User
+    """
+    avatar = None
+    try:
+        g = Gravatar(body.email)
+        avatar = g.get_image()
+    except Exception as e:
+        print(e)
+    new_user = User(**body.dict(), avatar=avatar)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+async def update_token(user: User, token: str | None, db: Session) -> None:
+    """
+    Creates an update token.
+
+    :param user: The user to create an update token for.
+    :type user: User
+    :param token: The token.
+    :type token: str | None
+    :param db: The database session.
+    :type db: Session
+    :return: None.
+    :rtype: None
+    """
+    user.refresh_token = token
+    db.commit()
+
+
+async def confirmed_email(email: str, db: Session) -> None:
+    """
+    Updates email confirmation status.
+
+    :param email: The email.
+    :type email: str
+    :param db: The database session.
+    :type db: Session
+    :return: None.
+    :rtype: None
+    """
+    user = await get_user_by_email(email, db)
+    user.confirmed = True
+    user.active = True
+    db.commit()
+
+
+async def update_avatar(email: str, url: str, db: Session) -> User:
+    """
+    Updates user's avatar.
+
+    :param email: The email.
+    :type email: str
+    :param url: The url of the avatar.
+    :type url: str
+    :param db: The database session.
+    :type db: Session
+    :return: The User with a new avatar.
+    :rtype: User
+    """
+    user = await get_user_by_email(email, db)
+    user.avatar = url
+    db.commit()
+    return user
