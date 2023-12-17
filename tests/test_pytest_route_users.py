2c276a6fb9dfbc46b1a4e71c23c8d403cbffafa2
lexxai <lexxai@users.noreply.github.com>
2023-12-17 02:43:22 +0200

    test_create_admin_user, test_repeat_create_same_user, test_create_general_user

--- tests/test_pytest_route_users.py
+++ tests/test_pytest_route_users.py
@@ -10,7 +10,7 @@ from src.conf import messages
 from src.database.models import User, Role
 
 
-def test_create_user(client, user, mock_ratelimiter, monkeypatch):
+def test_create_admin_user(client, user, mock_ratelimiter, monkeypatch):
     mock_send_email = MagicMock()
     get_image = MagicMock(return_value="MOC_AVATAR")
     add_task = MagicMock()
@@ -23,21 +23,50 @@ def test_create_user(client, user, mock_ratelimiter, monkeypatch):
     )
     assert response.status_code == 201, response.text
     data = response.json()
-    print(data)
+    # print(data)
     assert data["user"]["email"] == user.get("email")
     assert data["detail"] == messages.AUTH_USER_CREATED_CONFIRM
     assert data["user"]["role"] == 'admin'
     assert "id" in data["user"]
 
 
-# def test_repeat_create_user(client, user, mock_ratelimiter):
-#     response = client.post(
-#         "/api/auth/signup",
-#         json=user,
-#     )
-#     assert response.status_code == 409, response.text
-#     data = response.json()
-#     assert data["detail"] == "Account already exists"
+def test_repeat_create_same_user(client, user, mock_ratelimiter, monkeypatch):
+    mock_send_email = MagicMock()
+    get_image = MagicMock(return_value="MOC_AVATAR")
+    add_task = MagicMock()
+    monkeypatch.setattr("src.services.emails.send_email", mock_send_email)
+    monkeypatch.setattr("libgravatar.Gravatar.get_image", get_image)
+    monkeypatch.setattr("fastapi.BackgroundTasks.add_task", add_task)
+
+    response = client.post(
+        "/api/auth/signup",
+        json=user,
+    )
+    assert response.status_code == 409, response.text
+    data = response.json()
+    assert data["detail"] == messages.AUTH_ALREADY_EXIST
+
+def test_create_general_user(client, user, mock_ratelimiter, monkeypatch):
+    mock_send_email = MagicMock()
+    get_image = MagicMock(return_value="MOC_AVATAR")
+    add_task = MagicMock()
+    monkeypatch.setattr("src.services.emails.send_email", mock_send_email)
+    monkeypatch.setattr("libgravatar.Gravatar.get_image", get_image)
+    monkeypatch.setattr("fastapi.BackgroundTasks.add_task", add_task)
+    user_next = user.copy()
+    user_next["username"]  = "nextuser"
+    user_next["email"]  = "nextuser@example.com"
+    response = client.post(
+        "/api/auth/signup",
+        json=user_next,
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    # print(data)
+    assert data["user"]["email"] == user_next.get("email")
+    assert data["detail"] == messages.AUTH_USER_CREATED_CONFIRM
+    assert data["user"]["role"] == user_next.get("role")
+    assert "id" in data["user"]
 
 
 # def test_login_user_not_confirmed(client, user, mock_ratelimiter):
