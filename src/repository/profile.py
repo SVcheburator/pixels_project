a0ac2d3b01855c5e0dd9e0aa12e5f67a466a5367
lexxai <lexxai@users.noreply.github.com>
2023-12-17 19:02:23 +0200

    tune import path

--- src/repository/profile.py
+++ src/repository/profile.py
@@ -1,8 +1,8 @@
 from libgravatar import Gravatar
 from sqlalchemy import func
 from sqlalchemy.orm import Session
-from repository.users import clear_user_cache, get_user_by_username
 
+from src.repository.users import clear_user_cache, get_user_by_username
 from src.database.models import User, Role, Comment, Image
 from src.schemas import UpdateProfile, UserModel
 from src.services.auth import auth_service
@@ -22,20 +22,20 @@ async def read_profile(user: User, db: Session) -> dict:
     result = {}
     if user:
         # comments_count = db.query(Comment).filter(Comment.owner_id == user.id).count()
-        comments_count = db.query(func.count(Comment.owner_id  == user.id)).scalar()
-        images_count = db.query(func.count(Image.owner_id  == user.id)).scalar()
+        comments_count = db.query(func.count(Comment.owner_id == user.id)).scalar()
+        images_count = db.query(func.count(Image.owner_id == user.id)).scalar()
         result = {
-        "username": user.username,
-        "email": user.email,
-        "avatar": user.avatar,
-        "created_at": user.created_at,
-        "comments_count": comments_count,
-        "images_count": images_count
-    }
+            "username": user.username,
+            "email": user.email,
+            "avatar": user.avatar,
+            "created_at": user.created_at,
+            "comments_count": comments_count,
+            "images_count": images_count,
+        }
     return result
 
 
-async def update_profile(data: UpdateProfile, user: User, db: Session) -> bool| None:
+async def update_profile(data: UpdateProfile, user: User, db: Session) -> bool | None:
     """
     Retrieves a user profile.
 
@@ -54,7 +54,3 @@ async def update_profile(data: UpdateProfile, user: User, db: Session) -> bool| 
                 db.commit()
                 clear_user_cache(user)
                 return True
-
-
-
-
