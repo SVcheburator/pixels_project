1a2966e5c6e2a87426405080370b4babb9c688a7
AlexanderBgit <sb4mymail@gmail.com>
2023-12-18 04:27:59 +0200

    add transformation

--- src/routes/cloudinary.py
+++ src/routes/cloudinary.py
@@ -1,127 +1,50 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
-from fastapi.responses import FileResponse
-from PIL import Image as PILImage
-from io import BytesIO
-from datetime import datetime
-
-import uuid
-import qrcode
-import requests
 from sqlalchemy.orm import Session
-from src.database.models import Image as DBImage
 from src.database.db import get_db
-from src.services.cloudinary import CloudinaryService
-from src.services.auth import auth_service
-from src.schemas import UserDb
-import cloudinary.uploader
+from src.database.models import Image
+from src.services.cloudinary import cloudinary 
+import cloudinary
+from cloudinary.uploader import upload
+import cloudinary.api
+
+
+
+from fastapi import APIRouter, Depends
+from src.conf.config import settings
+
+class CloudinaryService:
+    cloudinary.config(
+        cloud_name=settings.cloudinary_name,
+        api_key=settings.cloudinary_api_key,
+        api_secret=settings.cloudinary_api_secret,
+        secure=True,
+    )
 
 cloud_router = APIRouter(prefix='/cloudinary')
 
 
 @cloud_router.get("/transformed_image/{image_id}")
-async def get_transformed_image(image_id: str, db: Session = Depends(get_db)):
-    image = get_image_by_id(db, image_id)
+def transform_and_update_image(image_id: str, angle: int = 45, db: Session = Depends(get_db)):
+    image = db.query(Image).filter(Image.url_original.contains(image_id)).first()
 
-    if not image:
-        raise HTTPException(status_code=404, detail='Image not found')
-
-    # Download the image from Cloudinary
-    cloudinary_url = image.url_original
-    response = requests.get(cloudinary_url)
-    print(cloudinary_url)
-
-    if response.status_code == 200:
-        file_path = f"uploads/{image_id}_original.jpg"
-        with open(file_path, "wb") as file:
-            file.write(response.content)
-
-        return file_path
-    raise HTTPException(status_code=response.status_code, detail='Failed to download original image')
-
-
-async def process_image(image_id: str,
-                        db: Session = Depends(get_db),
-                        current_user: UserDb = Depends(auth_service.get_current_user)):
-    # Step 1: Call get_transformed_image
-    original_image_path = await get_transformed_image(image_id, db)
-
-    # Step 2: Call transform with UploadFile and owner_id
-    owner_id = current_user.id
-    transformed_data = await transform(owner_id=owner_id, file=original_image_path, db=db)
-
-    return transformed_data
-
-
-async def transform(owner_id: int,
-                    file: UploadFile = File(...),
-                    description: str = "", db: Session = Depends(get_db)):
-
-    # Check if it's a file path or an UploadFile object
-    if isinstance(file, str):
-        # If it's a file path, read the content
-        with open(file, "rb") as file_content:
-            img_content = file_content.read()
-    else:
-        # If it's an UploadFile object, read the content
-        img_content = await file.read()
-
-    # Transform the image
-    image = PILImage.open(BytesIO(img_content))
-    image = image.resize((400, 400))
-    image = image.convert("RGB")
-    buffer = BytesIO()
-    image.save(buffer, format="JPEG")
-    transformed_image_data = buffer.getvalue()
-
-    # Generate a unique public_id for Cloudinary
-    public_id = f"transformed_image_{uuid.uuid4()}"
-
-    try:
-        print(f"Public ID before upload: {public_id}")
-
-        # Завантаження на Cloudinary
-        response = cloudinary.uploader.upload(
-            img_content,
-            public_id=public_id,
-            overwrite=True,
-            folder="transformed_image"
-        )
+    if image:
+        url_original = image.url_original
+        public_id = '/'.join(url_original.split('/')[7:])
         
-        print("Successfully uploaded to Cloudinary")
+        transformation = {"angle": angle}
 
-        # Get the URL of the transformed image from Cloudinary
+        # Завантажити трансформоване зображення на Cloudinary
+        response = upload(url_original, transformation=transformation, public_id=public_id)
+
         transformed_image_url = response['secure_url']
 
-        # Save information about the transformed image to the database
-        db_image = DBImage(
-            owner_id=owner_id,
-            url_original="Original image URL" if image.url_original else None,
-            url_transformed=transformed_image_url,
-            url_original_qr="Original image QR URL" if image.url_original_qr else None,
-            url_transformed_qr="Transformed image QR URL" if image.url_transformed_qr else None,
-            description=description,
-            updated_at=datetime.utcnow(),
-        )
-
-        db.add(db_image)
+        db.query(Image).filter(Image.url_original.contains(image_id)).update({"url_transformed": transformed_image_url})
         db.commit()
 
-        # Create QR code for the transformed image URL
-        qrcode_image = qrcode.make(transformed_image_url)
-        qrcode_data = qrcode_image.to_bytes()
+        print("1:", image_id)
+        # print("2:", Image.id)
+        print("Original Image URL:", url_original)
+        print("Transformed Image URL:", transformed_image_url)
 
-        return {
-            "public_id": public_id,
-            "transformed_image_url": transformed_image_url,
-            "qrcode_data": qrcode_data,
-        }
+        return {"message": f"Image transformed and updated successfully. Rotated by {angle} degrees.", "transformed_image_url": transformed_image_url}
 
-    except Exception as e:
-        print(f"Error uploading to Cloudinary: {e}")
-        raise HTTPException(status_code=500, detail="Failed to upload image to Cloudinary")
-
-
-
-# Assuming you have a function to get the image by ID from the database
-def get_image_by_id(db: Session, image_id: str):
-    return db.query(DBImage).filter(DBImage.id == image_id).first()
+    return {"error": "Image not found."}
