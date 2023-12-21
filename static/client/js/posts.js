d7c0cf96261ad6a41fab28ec92aab7a9c9404449
lexxai <lexxai@users.noreply.github.com>
2023-12-22 01:23:10 +0200

    tune album, tags, view, crop transformation on fly

--- static/client/js/posts.js
+++ static/client/js/posts.js
@@ -3,16 +3,46 @@ const post_template = document.getElementById("post_template");
 const page_next = document.getElementById("page_next");
 const page_prev = document.getElementById("page_prev");
 
+function image_thumb(img_url){
+  if (img_url && img_url.includes(".cloudinary.com")){
+    const search_txt ="/upload/";
+    const transformation = "c_fill_pad,g_auto,h_200,w_200"
+    const replace_txt ="/upload/"+transformation+"/";
+    return img_url.replace(search_txt, replace_txt);
+  }
+  return img_url;
+}
+
+function tags_convert(tags){
+  if (tags) {
+    const result = []
+    for (let index = 0; index < tags.length; index++) {
+      const element = tags[index];
+      if (element){
+        tag_a = document.createElement("a");
+        tag_a.href = "?tag="+element;
+        tag_a.innerHTML = "#"+element;
+        result.push(tag_a.outerHTML);
+      }
+    }
+    return result.join(" ");
+  }
+}
+
 function createPost(post) {
   const clone = post_template.content.cloneNode(true);
   clone.querySelector("p.card-text").innerText = post?.description;
   const img = clone.querySelector("img");
-  img.src = post?.url_original;
-  img.alt = post?.id;
+  img.src = image_thumb(post?.url_original);
+  img.alt = post?.id;  
+  const img_url = clone.querySelector("a.view_img");
+  img_url.href = post?.url_original;
   const date_cr = clone.querySelector("small.date-cr");
   if (post?.created_at) date_cr.innerHTML = post?.created_at;
   const tags = clone.querySelector("p.tags");
-  if (post?.tags) tags.innerHTML = post?.tags;
+  if (post?.tags) {
+    tags.innerHTML = tags_convert(post?.tags);
+  }
   post_content.appendChild(clone);
 }
 
@@ -119,7 +149,7 @@ get_posts = async () => {
 
   //setLoading(user_profile);
   const page = get_args();
-  const limit = 10;
+  const limit = 12;
   const offset = limit*page;
   const URL = `${BASE_URL}/posts/user/${user_id}?limit=${limit}&offset=${offset}`;
   const response = await fetch(URL, {
@@ -180,130 +210,130 @@ get_posts();
 // }, 15000);
 // setTimeout(()=>{get_cats},15000);
 
-const form = document.getElementById("form_username");
+// const form = document.getElementById("form_username");
 
-form?.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = document.querySelector('input[type="submit"]');
-  if (button) {
-    button.setAttribute("disabled", true);
-  }
-  const t = e.target;
-  const username = t.username.value;
-  const data = {
-    username: username,
-  };
-  const URL = `${BASE_URL}/api/users/me/`;
-  await fetch(URL, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (button) {
-        button.removeAttribute("disabled");
-      }
-      if (response.status >= 500) {
-        throw "ERROR STATUS: " + response.status;
-      }
-      if (response.status < 400) {
-        //window.location = "confirm.html";
-      }
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json);
-      detail = json?.detail;
-      err = "";
-      if (Array.isArray(detail)) {
-        for (const d of detail) {
-          const loc = d.loc[1];
-          const mgs = d.msg;
-          err = err + " " + loc + ": " + mgs;
-          console.log(d);
-        }
-      } else {
-        err = detail;
-      }
-      if (err) {
-        showMessage(err);
-      }
-    })
-    .catch((err) => {
-      console.log("ERROR", err);
-      showMessage(err);
-    });
-});
+// form?.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const button = document.querySelector('input[type="submit"]');
+//   if (button) {
+//     button.setAttribute("disabled", true);
+//   }
+//   const t = e.target;
+//   const username = t.username.value;
+//   const data = {
+//     username: username,
+//   };
+//   const URL = `${BASE_URL}/api/users/me/`;
+//   await fetch(URL, {
+//     method: "PATCH",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`,
+//     },
+//     body: JSON.stringify(data),
+//   })
+//     .then((response) => {
+//       if (button) {
+//         button.removeAttribute("disabled");
+//       }
+//       if (response.status >= 500) {
+//         throw "ERROR STATUS: " + response.status;
+//       }
+//       if (response.status < 400) {
+//         //window.location = "confirm.html";
+//       }
+//       return response.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//       detail = json?.detail;
+//       err = "";
+//       if (Array.isArray(detail)) {
+//         for (const d of detail) {
+//           const loc = d.loc[1];
+//           const mgs = d.msg;
+//           err = err + " " + loc + ": " + mgs;
+//           console.log(d);
+//         }
+//       } else {
+//         err = detail;
+//       }
+//       if (err) {
+//         showMessage(err);
+//       }
+//     })
+//     .catch((err) => {
+//       console.log("ERROR", err);
+//       showMessage(err);
+//     });
+// });
 
-const avatar_img = document.getElementById("avatar");
-const avatar_upload = document.getElementById("avatar_upload");
-const form_avatar = document.getElementById("form_avatar");
+// const avatar_img = document.getElementById("avatar");
+// const avatar_upload = document.getElementById("avatar_upload");
+// const form_avatar = document.getElementById("form_avatar");
 
-async function send_img(t) {
-  // e.preventDefault();
-  // const t = e.target;
-  //t.preventDefault();
-  //const  w =avatar_img.clientWidth ;
-  avatar_img.src = "";
-  //avatar_img.clientWidth=w;
-  avatar_img.src = "/static/client/images/loading.gif";
-  const formData = new FormData(t);
-  const URL = `${BASE_URL}/api/users/avatar/`;
-  await fetch(URL, {
-    method: "PATCH",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: formData,
-  })
-    .then((response) => {
-      if (response.status >= 500) {
-        throw "ERROR STATUS: " + response.status;
-      }
-      if (response.status < 400) {
-        //window.location = "confirm.html";
-      }
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json);
-      if (json?.avatar) {
-        avatar_img.src = json?.avatar;
-      }
-      detail = json?.detail;
-      err = "";
-      if (Array.isArray(detail)) {
-        for (const d of detail) {
-          const loc = d.loc[1];
-          const mgs = d.msg;
-          err = err + " " + loc + ": " + mgs;
-          console.log(d);
-        }
-      } else {
-        err = detail;
-      }
-      if (err) {
-        showMessage(err);
-      }
-    })
-    .catch((err) => {
-      console.log("ERROR", err);
-      showMessage(err);
-    });
-}
+// async function send_img(t) {
+//   // e.preventDefault();
+//   // const t = e.target;
+//   //t.preventDefault();
+//   //const  w =avatar_img.clientWidth ;
+//   avatar_img.src = "";
+//   //avatar_img.clientWidth=w;
+//   avatar_img.src = "/static/client/images/loading.gif";
+//   const formData = new FormData(t);
+//   const URL = `${BASE_URL}/api/users/avatar/`;
+//   await fetch(URL, {
+//     method: "PATCH",
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//     body: formData,
+//   })
+//     .then((response) => {
+//       if (response.status >= 500) {
+//         throw "ERROR STATUS: " + response.status;
+//       }
+//       if (response.status < 400) {
+//         //window.location = "confirm.html";
+//       }
+//       return response.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//       if (json?.avatar) {
+//         avatar_img.src = json?.avatar;
+//       }
+//       detail = json?.detail;
+//       err = "";
+//       if (Array.isArray(detail)) {
+//         for (const d of detail) {
+//           const loc = d.loc[1];
+//           const mgs = d.msg;
+//           err = err + " " + loc + ": " + mgs;
+//           console.log(d);
+//         }
+//       } else {
+//         err = detail;
+//       }
+//       if (err) {
+//         showMessage(err);
+//       }
+//     })
+//     .catch((err) => {
+//       console.log("ERROR", err);
+//       showMessage(err);
+//     });
+// }
 
-form_avatar.addEventListener("submit", send_img);
+// form_avatar.addEventListener("submit", send_img);
 
-avatar_img?.addEventListener("click", async (e) => {
-  e.preventDefault();
-  avatar_upload?.click();
-});
+// avatar_img?.addEventListener("click", async (e) => {
+//   e.preventDefault();
+//   avatar_upload?.click();
+// });
 
-avatar_upload?.addEventListener("change", (e) => {
-  e.preventDefault();
-  //form_avatar.submit();
-  send_img(form_avatar);
-});
+// avatar_upload?.addEventListener("change", (e) => {
+//   e.preventDefault();
+//   //form_avatar.submit();
+//   send_img(form_avatar);
+// });
